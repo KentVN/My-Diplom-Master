80d76ae9d5878488240d6129088dfae3bcaeeece
KentVN <vothanhquansh@gmail.com>
2019-10-24 21:15:29 +0300

    Update data and new files, new function..!

--- js/myjs.js
+++ js/myjs.js
@@ -138,6 +138,22 @@ function containerMenu(){
 		oldPrice : '174',
 		newPrice : '139',
 		static : 'sale'
+		},
+		{
+			imagesF: './photos/products/console1.png',
+			imagesS: './photos/products/console2.jpg',
+			name: 'Game Console Controller',
+			oldPrice: '1,100',
+			newPrice: '1,059',
+			static: '-20%'
+		},
+		{
+			imagesF: './photos/products/headphone1.png',
+			imagesS: './photos/products/headphone2.png',
+			name: 'White Solo 2 Wireless',
+			oldPrice: '1,100',
+			newPrice: '1,059',
+			static: 'HOT'
 		}
 	]
 
@@ -145,10 +161,13 @@ function containerMenu(){
 	function showLatest(){
 		let products = document.getElementsByClassName('rowProducts')[0];
 		let productsHTML = '';
-		latestProducts.map( (product) => {
+
+		for(let i = 0; i < 4; i++){
+			let product = latestProducts[i];
 			productsHTML += `<div class="boxProduct ">
 								<div class="product flex-row">
-									<img src="${product.imagesF}">
+									<img class="imgF" src="${product.imagesF}">
+									<img class="imgS" src="${product.imagesS}">
 									<div class="infoProduct flex-column">
 										<i>${product.brand}</i>
 										<b>${product.name}</b>
@@ -166,48 +185,54 @@ function containerMenu(){
 									</div>
 								</div>	
 							</div>`;
-		})
-	
+		}
+
 		products.innerHTML = productsHTML;
 	}
 
 	let bestProducts = [{
-			images: './photos/products/mobile1.png',
+			imagesF: './photos/products/mobile1.png',
+			imagesS: './photos/products/mobile2.png',
 			name: 'AA Smartphone XS',
 			oldPrice: '1,100',
 			newPrice: '1,059',
 			static: '-3%'
 		},
 		{
-			images: './photos/products/console1.png',
+			imagesF: './photos/products/console1.png',
+			imagesS: './photos/products/console2.jpg',
 			name: 'Game Console Controller',
 			oldPrice: '1,100',
 			newPrice: '1,059',
 			static: '-20%'
 		},
 		{
-			images: './photos/products/TV1.png',
+			imagesF: './photos/products/TV1.png',
+			imagesS: './photos/products/TV2.jpg',
 			name: 'AD QLed TV UltraHDn Curved',
 			oldPrice: '',
 			newPrice: '2,199',
 			static: 'OUT OF STOCK'
 		},
 		{
-			images: './photos/products/headphone1.png',
+			imagesF: './photos/products/headphone1.png',
+			imagesS: './photos/products/headphone2.png',
 			name: 'White Solo 2 Wireless',
 			oldPrice: '1,100',
 			newPrice: '1,059',
 			static: 'HOT'
 		},
 		{
-			images: './photos/products/watch1.png',
+			imagesF: './photos/products/watch1.png',
+			imagesS: './photos/products/watch2.png',
 			name: 'Amazfit SmartWatch',
 			oldPrice: '174',
 			newPrice: '139',
 			static: '-20%'
 		},
 		{
-			images: './photos/products/note1.jpg',
+			imagesF: './photos/products/note1.jpg',
+			imagesS: './photos/products/note2.jpg',
 			name: 'Smartphone Note 7',
 			oldPrice: '',
 			newPrice: '199',
@@ -222,8 +247,19 @@ function containerMenu(){
 		name : 'AA Laptop air',
 		oldPrice : '899',
 		newPrice : '739',
-		static : 'sale'
-	}]
+		static : 'sale',
+		value: 'recomment'
+	},
+	{
+		images: './photos/products/iphone11.png',
+		name: 'Iphone 11 Pro Max',
+		oldPrice: '1,200',
+		newPrice: '1,159',
+		static: '-3%',
+		brand : 'Apple',
+		value: 'hot'
+	}
+	]
 	
 
 	function showBestProducts(){
@@ -234,7 +270,8 @@ function containerMenu(){
 		bestProducts.map( (product) => {
 			productsHTML += `<div class="boxProduct ">
 								<div class="product flex-row">
-									<img src="${product.images}">
+									<img class="imgF" src="${product.imagesF}">
+									<img class="imgS" src="${product.imagesS}">
 									<div class="infoProduct flex-column">
 										<b>${product.name}</b>
 										<div class="price">
@@ -254,7 +291,8 @@ function containerMenu(){
 		});
 
 		bestSeller.map( (product) => {
-			bestSellerHTML += `<div class="boxProduct ">
+			if( product.value === 'recomment'){
+				bestSellerHTML += `<div class="boxProduct ">
 								<div class="product flex-column">
 									<img src="${product.imagesF}">
 									<div class="infoProduct flex-column">
@@ -273,6 +311,8 @@ function containerMenu(){
 									</div>
 								</div>	
 							</div>`;
+			}
+			
 		})
 
 		leftProducts.innerHTML = productsHTML;
@@ -364,8 +404,381 @@ function containerMenu(){
 		flexNav.innerHTML = flexNavHTML;
 	}
 
+
+
+	let latesProductsBox = [
+
+		{
+			imagesF: './photos/products/note1.jpg',
+			imagesS: './photos/products/note2.jpg',
+			name: 'Smartphone Note 7',
+			oldPrice: '',
+			newPrice: '199',
+			static: 'HOT',
+			brand: 'Xiaomi'
+		},
+
+		{
+			imagesF: './photos/products/smartWatch1.jpg',
+			imagesS: './photos/products/smartWatch2.jpg',
+			name: 'Smartwatch Fronier',
+			oldPrice: '239',
+			newPrice: '219',
+			static: 'SALE',
+			brand: 'Xiaomi'
+		}
+	]
+
+	latestProducts.map( (x) => {
+		latesProductsBox.unshift(x);
+	})
+
+	
+	function writeLatesProducts(){ // Write with HTML to display Lates Products on Screen !
+		const boxProducts = document.getElementsByClassName('boxProducts')[0];
+		const boxProductLeft = document.getElementsByClassName('boxProductLeft')[0];
+		let boxProductsHTML = '';
+		let leftProductHTML = '';
+		latesProductsBox.map( (product) => {
+			boxProductsHTML += `<div class="boxProduct ">
+								<div class="product flex-column">
+									<img class="imgF" src="${product.imagesF}">
+									<img class="imgS" src="${product.imagesS}">
+									
+									<div class="infoProduct flex-column">
+										<i>${product.brand}</i>
+										<b>${product.name}</b>
+										<div class="price">
+											<span class="old">$${product.oldPrice}.00</span>
+											<span class="new">$${product.newPrice}.00</span>
+										</div>
+									</div>
+									<div class="static">${product.static}</div>
+									<div class="iconsProduct">
+										<i class="fas fa-cart-arrow-down"></i>
+										<i class="far fa-heart"></i>
+										<i class="fas fa-random"></i>
+										<i class="far fa-eye"></i>
+									</div>
+								</div>	
+							</div>`;
+		})
+
+		bestSeller.map( (product) => {
+			if(product.value === 'hot'){
+				leftProductHTML += `<div class="boxProduct ">
+								<div class="product flex-column">
+									<img src="${product.images || product.imagesS}">
+									<div class="infoProduct flex-column">
+										<i>${product.brand}</i>
+										<b>${product.name}</b>
+										<div class="price">
+											<span class="old">$${product.oldPrice}.00</span>
+											<span class="new">$${product.newPrice}.00</span>
+										</div>
+									</div>	
+								</div>	
+							</div>`;
+			}
+		})
+		boxProductLeft.innerHTML = leftProductHTML;
+		boxProducts.innerHTML = boxProductsHTML;
+	}
+
+ function pushArr(arr1, arr2){
+ 	let arr3 = arr2.map( (x) => {
+ 		arr1.push(x);
+ 	})
+ 	return arr3;
+ }
+
+ let latestList = [];
+
+ pushArr(latestList, latestProducts);
+
+
+let trendingList = [
+	{
+
+		imagesF : './photos/products/laptop1.png',
+		imagesS : './photos/products/laptop2.png',
+		brand : 'Asus',
+		name : 'AA Laptop air',
+		oldPrice : '899',
+		newPrice : '739',
+		static : 'sale'
+		
+	},
+
+	{
+
+		imagesF : './photos/products/mobile1.png',
+		imagesS : './photos/products/mobile2.png',
+		brand : 'Apple',
+		name : 'AA Smartphone XS',
+		oldPrice : '1,100',
+		newPrice : '1,059',
+		static : 'sale'
+	},
+
+	{
+
+		imagesF : './photos/products/tabletPro1.png',
+		imagesS : './photos/products/tabletPro2.png',
+		brand : 'Apple',
+		name : 'BA Tablet Pro',
+		oldPrice : '',
+		newPrice : '999',
+		static : 'sale'
+	},
+
+	{
+
+		imagesF : './photos/products/headphoneSolo3-1.png',
+		imagesS : './photos/products/headphoneSolo3-2.png',
+		brand : 'Beat',
+		name : 'Headphone Solo 3',
+		oldPrice : '219',
+		newPrice : '189',
+		static : 'sale'
+	},
+	{
+
+		imagesF : './photos/products/headphoneSolo2-1.png',
+		imagesS : './photos/products/headphoneSolo2-2.png',
+		brand : 'Beat',
+		name : 'Headphone solo 2',
+		oldPrice : '',
+		newPrice : '190',
+		static : 'sale'
+	},
+	{
+
+		imagesF : './photos/products/fronier-1.jpg',
+		imagesS : './photos/products/fronier-2.jpg',
+		brand : 'Fronier',
+		name : 'Smartwatch Fronier',
+		oldPrice : '239',
+		newPrice : '219',
+		static : 'sale'
+	},
+
+];
+
+
+let topSellingList = [
+
+
+	{
+
+		imagesF : './photos/products/mobile1.png',
+		imagesS : './photos/products/mobile2.png',
+		brand : 'Apple',
+		name : 'AA Smartphone XS',
+		oldPrice : '1,100',
+		newPrice : '1,059',
+		static : 'sale'
+	},
+
+	{
+
+		imagesF : './photos/products/HeadphonePods-1.png',
+		imagesS : './photos/products/headphonePods-2.jpg',
+		brand : 'Apple',
+		name : 'Headphone Pods',
+		oldPrice : '175',
+		newPrice : '149',
+		static : 'sale'
+		
+	},
+
+	{
+
+		imagesF : './photos/products/headphoneSolo2-1.png',
+		imagesS : './photos/products/headphoneSolo2-2.png',
+		brand : 'Beat',
+		name : 'Headphone solo 2',
+		oldPrice : '',
+		newPrice : '190',
+		static : 'sale'
+	},
+
+	{
+
+		imagesF : './photos/products/headphone1.png',
+		imagesS : './photos/products/headphone2.png',
+		brand : 'Beat',
+		name : 'White Solo 2 Wireless',
+		oldPrice : '',
+		newPrice : '129',
+		static : 'sale'
+	},
+	{
+
+		imagesF : './photos/products/smartphoneXR-1.png',
+		imagesS : './photos/products/smartphoneXR-2.png',
+		brand : 'Apple',
+		name : 'AB Smartphone XR',
+		oldPrice : '799',
+		newPrice : '749',
+		static : 'sale'
+	},
+	{
+
+		imagesF : './photos/products/smartWatch1.jpg',
+		imagesS : './photos/products/smartWatch2.jpg',
+		brand : 'Fronier',
+		name : 'Amazfit SmartWatch',
+		oldPrice : '174',
+		newPrice : '139',
+		static : 'sale'
+	},
+
+];
+
+
+let popularList = [
+	
+	{
+		imagesF : './photos/products/headphone1.png',
+		imagesS : './photos/products/headphone2.png',
+		brand : 'Beat',
+		name : 'White Solo 2 Wireless',
+		oldPrice : '',
+		newPrice : '129',
+		static : 'sale'
+	},
+
+	{
+
+		imagesF : './photos/products/swWaterproof-1.png',
+		imagesS : './photos/products/swWaterproof-2.png',
+		brand : 'Apple',
+		name : 'Smart Watches',
+		oldPrice : '1,100',
+		newPrice : '1,059',
+		static : 'sale'
+	},
+
+	{
+
+		imagesF : './photos/products/cameraNx201-1.png',
+		imagesS : './photos/products/cameraNx201-2.png',
+		brand : 'Rec',
+		name : 'Camera Nx-201',
+		oldPrice : '1,999',
+		newPrice : '1,799',
+		static : 'sale'
+		
+	},
+
+	{
+
+		imagesF : './photos/products/laptopG4556-1.png',
+		imagesS : './photos/products/laptopG4556-2.png',
+		brand : 'MSI',
+		name : 'BA Laptop 15.6" Intel G4556',
+		oldPrice : '599',
+		newPrice : '499',
+		static : 'sale'
+	},
+
+	{
+
+		imagesF : './photos/products/WWflash-1.jpg',
+		imagesS : './photos/products/WWflash-2.jpg',
+		brand : 'USB',
+		name : 'WW Flash 16GB USB 3.1',
+		oldPrice : '',
+		newPrice : '100',
+		static : 'sale'
+	},
+
+	{
+
+		imagesF : './photos/products/consoleEdition-1.png',
+		imagesS : './photos/products/consoleEdition-2.png',
+		brand : 'Sony',
+		name : 'GameConsole Run Edition',
+		oldPrice : '',
+		newPrice : '299',
+		static : 'sale'
+	}
+	
+
+];
+
+
+function writeProducts(columnArr, box){
+	let boxHTML = '';
+	let boxProducts = document.getElementsByClassName(box)[0];
+	columnArr.map( (product) => {
+		boxHTML += `<div class="boxProduct ">
+								<div class="product flex-row">
+									<img class="imgF" src="${product.imagesF}">
+									<img class="imgS" src="${product.imagesS}">
+									
+									<div class="infoProduct flex-column">
+										<i>${product.brand}</i>
+										<b>${product.name}</b>
+										<div class="price">
+											<span class="old">$${product.oldPrice}.00</span>
+											<span class="new">$${product.newPrice}.00</span>
+										</div>
+									</div>
+									<div class="static">${product.static}</div>
+									<div class="iconsProduct">
+										<i class="fas fa-cart-arrow-down"></i>
+										<i class="far fa-heart"></i>
+										<i class="fas fa-random"></i>
+										<i class="far fa-eye"></i>
+									</div>
+								</div>	
+							</div>`;
+
+		boxProducts.innerHTML = boxHTML;
+		
+	})
+}
+
+
+function btnClick(){
+	let next = document.getElementsByClassName('arrowRight');
+	let prev = document.getElementsByClassName('arrowLeft');
+	let recommentsBox = document.getElementsByClassName('recommentsBox')
+
+	for(let i = 0; i < next.length; i++){
+
+		let position = 0;
+
+		next[i].addEventListener("click",function(){
+			if( position < 1){
+				recommentsBox[i].style.transform = "translateX(-100%)";
+				position++;
+			}
+		})
+
+		prev[i].addEventListener("click", function(){
+			if(position = 1){
+				recommentsBox[i].style.transform = "translateX(0%)";
+				position--;
+			}
+		})
+	}
+}
+
+
+
+
+
 containerMenu();
 showLatest();
 showBestProducts();
 showListMenu();
 writeFlexNav();
+writeLatesProducts();
+writeProducts(latestList, "boxLatest");
+writeProducts(trendingList,"boxTrending");
+writeProducts(topSellingList, "boxTopSelling");
+writeProducts(popularList, "boxPopular");
+btnClick();
